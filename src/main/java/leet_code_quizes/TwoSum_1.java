60cd978474ccd593bcebe985d433cd204c8955c6
wincelee <aguerwinceville@gmail.com>
2022-07-16 19:37:12 +0300

    leetcode sunday changes

--- src/main/java/leet_code_quizes/TwoSum_1.java
+++ src/main/java/leet_code_quizes/TwoSum_1.java
@@ -6,17 +6,19 @@ import java.util.HashMap;
 public class TwoSum_1 {
 
     public static void main(String[] args) {
-        //int[] nums = {2,7,11,15};
-        int[] nums = {3,3};
+        int[] nums = {2,7,11,15};
+        int[] nums1 = {3,3};
 
         //int target = 9;
         int target = 6;
 
-        System.out.println("TwoSum: " + Arrays.toString(twoSum(nums, target)));
+        System.out.println("TwoSumReturnIndexes: " + Arrays.toString(twoSumReturnIndexes(nums1, target)));
+        System.out.println("TwoSumReturnElements: " + Arrays.toString(twoSumReturnElements(nums1, target)));
+        System.out.println("AreThereElementsThatAddUpToTarget: " + twoSumReturnIfElementsInArrayAddUpToTarget(nums1, target));
 
     }
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumReturnIndexes(int[] nums, int target) {
 
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
@@ -28,5 +30,33 @@ public class TwoSum_1 {
         }
 
         return null;
+    }
+
+    public static int[] twoSumReturnElements(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i =0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{target-nums[i], nums[i]};
+            }
+            map.put(nums[i],i);
+        }
+
+        return null;
+    }
+
+    public static boolean twoSumReturnIfElementsInArrayAddUpToTarget(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i =0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+
+        return false;
     }
 }
