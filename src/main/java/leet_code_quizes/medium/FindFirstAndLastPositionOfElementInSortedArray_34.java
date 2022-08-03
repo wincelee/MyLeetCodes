70d9416ec180a2c989a4e526844fddf3ff745fc7
wincelee <aguerwinceville@gmail.com>
2022-08-03 09:19:08 +0300

    Changes on first and last positios of element in sorted array

--- src/main/java/leet_code_quizes/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
+++ src/main/java/leet_code_quizes/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -1,8 +1,8 @@
 package leet_code_quizes.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.google.gson.Gson;
+
+import java.util.*;
 
 public class FindFirstAndLastPositionOfElementInSortedArray_34 {
 
@@ -11,6 +11,7 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
         int[] nums1 = {5,7,7,8,8,10};
 
         System.out.println("Nums1FirstAndLastPositionOfTarget: " + Arrays.toString(searchRange(nums1, 8)));
+        // System.out.println("Nums1FirstAndLastPositionOfTarget2: " + Arrays.toString(searchRange2(nums1, 8)));
 
     }
 
@@ -19,19 +20,77 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
         List<Integer> positions = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++){
-            if(i==target){
+
+
+            if(nums[i]==target){
                 positions.add(i);
             }
+        }
+
+        if (positions.isEmpty()){
+            return new int[]{-1, -1};
+        }
+
+
+        if(positions.size() == 1){
+
+            int[] res = new int[2];
+
+            for(int i = 0; i < positions.size(); i++) {
+
+                if(positions.size() == 1){
+                    Arrays.fill(res, positions.get(i));
+                }
+
+                res[i] = positions.get(i);
+
+            }
+
+            return res;
+
         }
 
         int[] res = new int[positions.size()];
 
         for(int i = 0; i < positions.size(); i++) {
 
+
             res[i] = positions.get(i);
 
         }
 
         return res;
+    }
+
+    public static int[] searchRange2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target)){
+                res.add(map.get(nums[i]));
+            }
+
+            map.put(nums[i], i);
+        }
+
+        System.out.println("ResList" + new Gson().toJson(res));
+
+        if(res.isEmpty()){
+            return new int[]{-1, -1};
+        }
+
+        int[] temp = new int[res.size()];
+
+        for(int i = 0; i < res.size(); i++) {
+
+            temp[i] = res.get(i);
+
+        }
+
+        return temp;
+
     }
 }
