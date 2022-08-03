e9a1db20a10e9bacb7c1b0947adb1b44317b56c7
emmanjorodongo <emmanjorodongo@gmail.com>
2022-08-03 16:31:18 +0300

    FindingFirstAndLastPositionOfElementsInSortedArray

--- src/main/java/leet_code_quizes/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
+++ src/main/java/leet_code_quizes/medium/FindFirstAndLastPositionOfElementInSortedArray_34.java
@@ -10,87 +10,70 @@ public class FindFirstAndLastPositionOfElementInSortedArray_34 {
 
         int[] nums1 = {5,7,7,8,8,10};
 
-        System.out.println("Nums1FirstAndLastPositionOfTarget: " + Arrays.toString(searchRange(nums1, 8)));
-        // System.out.println("Nums1FirstAndLastPositionOfTarget2: " + Arrays.toString(searchRange2(nums1, 8)));
+        System.out.println("Nums1FirstAndLastPositionOfTarget: " + Arrays.toString(searchRangeUsingList(nums1, 8)));
+        //System.out.println("Nums1FirstAndLastPositionOfTarget: " + Arrays.toString(searchRangeUsingTwoPointers(nums1, 8)));
 
     }
 
-    public static int[] searchRange(int[] nums, int target) {
+    public static int[] searchRangeUsingList(int[] nums, int target) {
 
-        List<Integer> positions = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++){
-
-
-            if(nums[i]==target){
-                positions.add(i);
-            }
-        }
-
-        if (positions.isEmpty()){
-            return new int[]{-1, -1};
-        }
-
-
-        if(positions.size() == 1){
-
-            int[] res = new int[2];
-
-            for(int i = 0; i < positions.size(); i++) {
-
-                if(positions.size() == 1){
-                    Arrays.fill(res, positions.get(i));
-                }
-
-                res[i] = positions.get(i);
-
-            }
-
-            return res;
-
-        }
-
-        int[] res = new int[positions.size()];
-
-        for(int i = 0; i < positions.size(); i++) {
-
-
-            res[i] = positions.get(i);
-
-        }
-
-        return res;
-    }
-
-    public static int[] searchRange2(int[] nums, int target) {
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        List<Integer> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(target)){
-                res.add(map.get(nums[i]));
+            if(nums[i] == target){
+                list.add(i);
             }
-
-            map.put(nums[i], i);
         }
 
-        System.out.println("ResList" + new Gson().toJson(res));
-
-        if(res.isEmpty()){
+        if(list.isEmpty()){
             return new int[]{-1, -1};
         }
 
-        int[] temp = new int[res.size()];
+        return new int[]{list.get(0), list.get(list.size()-1)};
 
-        for(int i = 0; i < res.size(); i++) {
+    }
 
-            temp[i] = res.get(i);
+
+    public static int[] searchRangeUsingTwoPointers(int[] nums, int target) {
+
+        int[] res = new int[2];
+
+
+        int lp = 0, rp = nums.length-1, firstPos =0, lastPos =0;
+
+        boolean incLp = true, decRp = true;
+
+
+        while (lp < rp){
+
+            if(nums[lp] == target){
+                firstPos=lp;
+                incLp = false;
+            }else{
+                if (incLp){
+                    lp++;
+                }
+            }
+
+            if(nums[rp]==target){
+                lastPos=rp;
+                decRp = false;
+            }else{
+                if(decRp){
+                    rp--;
+                }
+            }
 
         }
 
-        return temp;
+        System.out.println("FirstPos" + firstPos);
+        System.out.println("LastPos" + lastPos);
+
+        /*if(firstPos!=0 && lastPos!=0){
+            return new int[]{firstPos, lastPos};
+        }
+*/
+        return new int[]{-1, -1};
 
     }
 }
