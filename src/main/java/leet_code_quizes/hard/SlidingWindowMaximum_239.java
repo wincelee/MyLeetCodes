72979f31f99dc39f20cf03d0b9a9d9c0e526ae81
emmanjorodongo <emmanjorodongo@gmail.com>
2022-08-23 20:05:46 +0300

    sliding window maximum

--- src/main/java/leet_code_quizes/hard/SlidingWindowMaximum_239.java
+++ src/main/java/leet_code_quizes/hard/SlidingWindowMaximum_239.java
@@ -10,8 +10,51 @@ public class SlidingWindowMaximum_239 {
         int[] nums1 = {1,3,-1,-3,5,3,6,7};
         int k1 = 3;
 
-        System.out.println("MaxSlidingWindowsNumsK1" +
-                Arrays.toString(maxSlidingWindow(nums1, k1)));
+        /*System.out.println("MaxSlidingWindowsNumsK1" +
+                Arrays.toString(maxSlidingWindow(nums1, k1)));*/
+
+        System.out.println("MaxSlidingWindows2NumsK1" +
+                Arrays.toString(maxSlidingWindow2(nums1, k1)));
+
+    }
+
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
+
+        List<Integer> resList = new ArrayList<>();
+
+        int firstIndex = 0;
+
+        for(int i =0; i < nums.length-k+1; i++){
+
+
+            for(int j = firstIndex+1; j <= i + k -1; j++){
+
+                /*if(nums[j] > tempMax){
+                    max= nums[j];
+                    resList.add(nums[j]);
+                }*/
+
+                if(nums[j] > tempMax){
+
+                    resList.add(nums[j]);
+                }
+
+                System.out.println("ResList: " + resList);
+
+            }
+
+            System.out.println("LoopExited: " + true);
+            firstIndex++;
+
+        }
+
+       int[] res = new int[resList.size()] ;
+
+       for (int i = 0; i < resList.size(); i++){
+           res[i] = resList.get(i);
+       }
+
+       return  res;
 
     }
 
