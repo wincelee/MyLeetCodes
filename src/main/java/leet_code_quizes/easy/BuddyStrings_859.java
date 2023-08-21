2297ff6395ca3040b5aa6a08aee03b04eed216dd
wincelee <aguerwinceville@gmail.com>
2023-08-21 17:19:05 +0300

    sum of unique elements 1748

--- src/main/java/leet_code_quizes/easy/BuddyStrings_859.java
+++ src/main/java/leet_code_quizes/easy/BuddyStrings_859.java
@@ -4,8 +4,10 @@ public class BuddyStrings_859 {
 
     public static void main(String[] args) {
         String s1 = "ab", s2 = "ba";
+        String s11 = "abcde", s22 = "abdce";
 
-        System.out.println("BuddyStrings: " + buddyStrings(s1, s2));
+        // System.out.println("BuddyStrings: " + buddyStrings(s1, s2));
+        System.out.println("BuddyStrings: " + buddyStringsTwo(s1, s2));
     }
 
     public static boolean buddyStrings(String s, String goal) {
@@ -21,5 +23,27 @@ public class BuddyStrings_859 {
         }
 
         return new String(chArr).equals(goal);
+    }
+    public static boolean buddyStringsTwo(String s, String goal) {
+
+        char[] chArr = s.toCharArray();
+
+        for (int i = 0; i < s.length()-1; i++){
+            //performLeftToRight();
+        }
+
+        return new String(chArr).equals(goal);
+    }
+
+    public static char[] performLeftToRight(char[] chArr, int i, int j){
+
+
+
+        /*for (int i = 0; i < chArr.length -1; i++){
+            char temp = chArr[i];
+            chArr[i] = chArr[i+1];
+        }*/
+
+        return chArr;
     }
 }
---
+++ src/main/java/leet_code_quizes/easy/SumOfUniqueElements_1748.java
@@ -0,0 +1,79 @@
+package leet_code_quizes.easy;
+
+import classes.Config;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SumOfUniqueElements_1748 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,2};
+        int[] nums1 = {1,1,1,1,1};
+
+        //System.out.println("SumOfUniqueElements: " + sumOfUnique(nums));
+        System.out.println("SumOfUniqueElements: " + sumOfUniqueUsingTwoLoops(nums));
+
+    }
+
+    public static int sumOfUnique(int[] nums) {
+        Set<Integer> mSet = new HashSet<>();
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num: nums){
+            /*if (map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            }else{
+                map.put(num, 1);
+            }*/
+            mSet.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        if (mSet.size() == 0){
+            return 0;
+        }
+
+        AtomicInteger sum = new AtomicInteger(0);
+
+        map.forEach((k,v) ->{
+            if (v == 1){
+                  sum.addAndGet(k);
+                 // sum.set(sum.get() + k);
+                System.out.println(Config.ANSI_YELLOW + "IfElseCalled:: " + k);
+            }
+        });
+
+
+        return sum.get();
+    }
+
+    public static int sumOfUniqueUsingTwoLoops(int[] nums) {
+        int sum = 0, count = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            count = 1;
+
+            for (int j = 0; j < nums.length; j++){
+                if (nums[i] == nums[j] && i != j){
+
+                    System.out.println(Config.ANSI_RED + "CurrentNumsOfI: " + nums[i]);
+                    System.out.println(Config.ANSI_YELLOW + "CurrentNumsOfJ: " + nums[j]);
+
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 1){
+                sum += nums[i];
+            }
+        }
+
+        return sum;
+    }
+}
